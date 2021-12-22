1c4210f45a4b83c2312642235a69063f21e33d7c
Dawid Nych <dawid.nych@gmail.com>
2021-12-22 02:39:00 +0100

    Heroku fix

---
+++ Procfile
@@ -0,0 +1,2 @@
+web: gunicorn recruitment_task.wsgi
+release: python manage.py migrate
--- recruitment_task/settings.py
+++ recruitment_task/settings.py
@@ -13,6 +13,8 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 import os
 from pathlib import Path
 
+import django_heroku
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 STATIC_ROOT = BASE_DIR / "staticfiles"
@@ -125,3 +127,6 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
